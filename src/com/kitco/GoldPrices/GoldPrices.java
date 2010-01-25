b3d33e71f5a35643b158b9d0f6712cb75885b961
diafygi <diafygi@ac9dd53c-057c-11df-8861-21795b59afe9>
2010-01-25 19:08:03 +0000

    Added primitive logging, refresh button, does not compile

--- src/com/kitco/GoldPrices/GoldPrices.java
+++ src/com/kitco/GoldPrices/GoldPrices.java
@@ -13,6 +13,9 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -31,12 +34,22 @@ public class GoldPrices extends Activity {
 	
 	String[] table_data = new String[13];
 	Bitmap[] charts = new Bitmap[8];
+	Integer loaded = null;
 	
 	private Handler handler = new Handler() {
 		@Override
 		public void handleMessage(Message msg) {
+			
+			Log.v("kitco","handler received message, finding progressbar widget");
+			
 			ProgressBar pg = (ProgressBar)findViewById(R.id.progressbar);
+			
+			Log.v("kitco","setting progressbar widget to invisible");
+			
 			pg.setVisibility(8);
+			
+			Log.v("kitco","updating layout to include table and chart data");
+			
 			updateViews();
 		}
 	};
@@ -44,8 +57,32 @@ public class GoldPrices extends Activity {
 	private void updateData() {
 		Thread t = new Thread() {
 			public void run() {
-				table_data = updateTable();
-				charts = updateCharts();
+				
+				Log.v("kitco","created new thread for updateData");
+				
+				//Check to see if data is already loaded
+				if(loaded == null) {
+					
+					Log.v("kitco","updating table");
+					
+					//Get updates to table
+					table_data = updateTable();
+					
+					Log.v("kitco","table updated");
+					Log.v("kitco","updating charts");
+					
+					//Get updates to charts
+					charts = updateCharts();
+					
+					Log.v("kitco","charts updated");
+					
+					//Mark as loaded
+					loaded = 1;
+				}
+				
+				Log.v("kitco","sending message to handler that updates are done");
+				
+				//Send signal if downloads complete
 				handler.sendMessage(Message.obtain());
 			}
 		};
@@ -58,23 +95,52 @@ public class GoldPrices extends Activity {
         super.onCreate(icicle);
         setContentView(R.layout.main);
         
+        Log.v("kitco","calling updateDate from onCreate");
         updateData();
+        
+        Button refresh_button = (Button)findViewById(R.id.refresh);
+        
+        refresh_button.setOnClickListener(new OnClickListener() {
+        	public void onClick(View v) {
+        		
+        		Log.v("kitco","refresh button clicked, making progress bar visible");
+        		
+        		//Make progressbar visible
+        		ProgressBar pg = (ProgressBar)findViewById(R.id.progressbar);
+        		pg.setVisibility(0);
+        		
+        		Log.v("kitco","force reloading data");
+        		
+        		//Force reload data
+        		loaded = null;
+        		updateData();
+        	}
+        });
     }
     
     private void updateViews() {
-        TextView bidask_label = (TextView)findViewById(R.id.bidask_label);
+    	
+    	Log.v("kitco","finding table labels");
+    	
+    	//Find table label boxes
+    	TextView bidask_label = (TextView)findViewById(R.id.bidask_label);
         TextView lowhigh_label = (TextView)findViewById(R.id.lowhigh_label);
         TextView change_label = (TextView)findViewById(R.id.change_label);
         TextView monthchange_label = (TextView)findViewById(R.id.monthchange_label);
         TextView yearchange_label = (TextView)findViewById(R.id.yearchange_label);
+        
+        Log.v("kitco","setting table label text");
+        
+        //Set table labels
         bidask_label.setText("Bid/Ask");
         lowhigh_label.setText("Low/High");
         change_label.setText("Change");
         monthchange_label.setText("30daychg");
         yearchange_label.setText("1yearchg");
-
         
-    	
+        Log.v("kitco","finding table info");
+        
+        //Get table data boxes
         TextView market_status = (TextView)findViewById(R.id.market_status);
         TextView market_time = (TextView)findViewById(R.id.market_time);
         TextView time = (TextView)findViewById(R.id.time);
@@ -89,11 +155,15 @@ public class GoldPrices extends Activity {
         TextView yearchange = (TextView)findViewById(R.id.yearchange);
         TextView yearchange_percent = (TextView)findViewById(R.id.yearchange_percent);
         
+        Log.v("kitco","setting table info");
+        
+        //Set market status color
         if(table_data[0]=="SPOT MARKET IS OPEN")
         	market_status.setTextColor(0xff00af00);
         else
         	market_status.setTextColor(0xffff0000);
         
+        //Set table data
         market_status.setText(table_data[0]);
         market_time.setText(table_data[1]);
         time.setText(table_data[2]);
@@ -108,8 +178,9 @@ public class GoldPrices extends Activity {
         yearchange.setText(table_data[11]);
         yearchange_percent.setText(table_data[12]);
         
-
-
+        Log.v("kitco","finding chart locations");
+        
+        //Get chart boxes
         ImageView chart_3day = (ImageView)findViewById(R.id.chart_3day);
         ImageView chart_nyspot = (ImageView)findViewById(R.id.chart_nyspot);
         ImageView chart_30day = (ImageView)findViewById(R.id.chart_30day);
@@ -119,6 +190,9 @@ public class GoldPrices extends Activity {
         ImageView chart_5year = (ImageView)findViewById(R.id.chart_5year);
         ImageView chart_10year = (ImageView)findViewById(R.id.chart_10year);
         
+        Log.v("kitco","setting chart images");
+        
+        //Set charts
         chart_3day.setImageBitmap(charts[0]);
         chart_nyspot.setImageBitmap(charts[1]);
         chart_30day.setImageBitmap(charts[2]);
@@ -133,8 +207,12 @@ public class GoldPrices extends Activity {
     private String[] updateTable() {
     	String[] tabledata = new String[13];
     	
+    	Log.v("kitco","grabbing webpage from kitco.com");
+    	
         //Get webpage data for prices
         String webpage = downloadHTML("http://www.kitco.com/");
+        
+        Log.v("kitco","finding location of table start and end on webpage");
         
         //Parse webpage data for values
         //Find range of data
@@ -145,69 +223,93 @@ public class GoldPrices extends Activity {
         Integer i = 0;
         Integer o = 0;
 
+        Log.v("kitco","getting market status");
+        
         //Get market status
-        if(data.contains("SPOT MARKET IS OPEN")) {
+        if(data.contains("SPOT MARKET IS OPEN"))
         	tabledata[0]="SPOT MARKET IS OPEN";
-        	b = 4;}
-        else {
-        	tabledata[0]="SPOT MARKET IS OPEN";
-        	b = 3;}
+        else
+        	tabledata[0]="SPOT MARKET IS CLOSED";
 
+        Log.v("kitco","getting market time");
+        
         //Get market time
-        i = data.indexOf("SPOT MARKET");
-        for(int n=0; n<b; n++){i = data.indexOf(">",i+1);}
+        i = data.indexOf("SPOT MARKET IS", start);
+        i = data.indexOf("<font face=\"Verdana, Arial, Helvetica, sans-serif\" size=\"1\">", i);
         o = data.indexOf("</font>",i);
-        tabledata[1]=data.substring(i+1,o);
+        tabledata[1]=data.substring(i+60,o);
 
+        Log.v("kitco","getting current time");
+        
         //Get time
         i = data.indexOf("<!--date -->");
         o = data.indexOf("</font>",i);
         tabledata[2]=data.substring(i+12,o);
+        
+        Log.v("kitco","getting bid price");
         
         //Get bid
         i = data.indexOf("<td align=\"center\" bgcolor=\"#f3f3e4\">",o);
         o = data.indexOf("</td>",i);
         tabledata[3]=data.substring(i+37,o);
         
+        Log.v("kitco","getting ask price");
+        
         //Get ask
         i = data.indexOf("<td align=\"center\" bgcolor=\"#f3f3e4\">",o);
         o = data.indexOf("</td>",i);
         tabledata[4]=data.substring(i+37,o);
+        
+        Log.v("kitco","getting low price");
         
         //Get low
         i = data.indexOf("<td align=\"center\">",o);
         o = data.indexOf("</td>",i);
         tabledata[5]=data.substring(i+19,o);
         
+        Log.v("kitco","getting high price");
+        
         //Get high
         i = data.indexOf("<td align=\"center\">",o);
         o = data.indexOf("</td>",i);
         tabledata[6]=data.substring(i+19,o);
+        
+        Log.v("kitco","getting change price");
         
         //Get change
         i = data.indexOf("<td align=\"center\" bgcolor=\"#f3f3e4\">",o);
         o = data.indexOf("</font>",i);
         tabledata[7]=data.substring(i+57,o);
         
+        Log.v("kitco","getting change percent");
+        
         //Get change_percent
         i = data.indexOf("<td align=\"center\" bgcolor=\"#f3f3e4\">",o);
         o = data.indexOf("</font>",i);
         tabledata[8]=data.substring(i+57,o);
+        
+        Log.v("kitco","getting 30 day change");
         
         //Get 30daychg
         i = data.indexOf("<td align=\"center\">",o);
         o = data.indexOf("</font>",i);
         tabledata[9]=data.substring(i+39,o);
         
+        Log.v("kitco","getting 30 day change percent");
+        
         //Get 30daychg_percent
         i = data.indexOf("<td align=\"center\">",o);
         o = data.indexOf("</font>",i);
         tabledata[10]=data.substring(i+39,o);
         
+        Log.v("kitco","getting 1 year change");
+        
         //Get 1yearchg
         i = data.indexOf("<td align=\"center\" bgcolor=\"#f3f3e4\">",o);
         o = data.indexOf("</font>",i);
         tabledata[11]=data.substring(i+57,o);
+        
+        Log.v("kitco","getting 1 year change percent");
         
       	//Get 1yearchg_percent
         i = data.indexOf("<td align=\"center\" bgcolor=\"#f3f3e4\">",o);
@@ -219,7 +321,9 @@ public class GoldPrices extends Activity {
     
     private Bitmap[] updateCharts() {
     	Bitmap[] charts = new Bitmap[8];
-    	        
+    	
+    	Log.v("kitco","set urls for chart images");
+    	
         //Set chart urls
         String url_chart_3day = "http://www.kitco.com/images/live/gold.gif";
         String url_chart_nyspot = "http://www.kitco.com/images/live/nygold.gif";
@@ -229,6 +333,8 @@ public class GoldPrices extends Activity {
         String url_chart_1year = "http://www.kitco.com/LFgif/au0365nyb.gif";
         String url_chart_5year = "http://www.kitco.com/LFgif/au1825nyb.gif";
         String url_chart_10year = "http://www.kitco.com/LFgif/au3650nyb.gif";
+        
+        Log.v("kitco","download chart images");
         
         //Download chart images
         charts[0] = downloadImage(url_chart_3day);
@@ -240,11 +346,16 @@ public class GoldPrices extends Activity {
         charts[6] = downloadImage(url_chart_5year);
         charts[7] = downloadImage(url_chart_10year);
         
+        Log.v("kitco","done downloading chart images");
+        
         return charts;
     }
     
     //Downloads an image from a specified url
     public Bitmap downloadImage(String url) {
+    	
+    	Log.v("kitco","downloading chart image from: "+url);
+    	
     	URL ChartUrl =null;
     	Bitmap image = null;
     	try {ChartUrl= new URL(url);}
@@ -256,11 +367,17 @@ public class GoldPrices extends Activity {
     		image = BitmapFactory.decodeStream(imagestream);
     		}
     	catch(IOException e) {e.printStackTrace();}
+    	
+    	Log.v("kitco","finished downloading chart image from: "+url);
+    	
 		return image;
     }
     
     //Downloads an html page from a specified url
     public String downloadHTML(String url) {
+    	
+    	Log.v("kitco","downloading html page from: "+url);
+    	
     	URL ChartUrl =null;
     	String page = null;
     	StringBuffer sb = new StringBuffer();
@@ -275,6 +392,9 @@ public class GoldPrices extends Activity {
     			sb.append(page).append("\n");}
     		}
     	catch(IOException e) {e.printStackTrace();}
+    	
+    	Log.v("kitco","finished downloading html page from: "+url);
+    	
 		return sb.toString();
     }
 }
